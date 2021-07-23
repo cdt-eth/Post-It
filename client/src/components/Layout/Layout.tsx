3efa7ededd678c5c95fa1a64f26f90b5afd8b44b
christianturner <christiandavidphoto@gmail.com>
2021-07-23 15:37:25 -0400

    Layout styles

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,10 +1,24 @@
+import { makeStyles } from "@material-ui/core";
 import { ReactElement } from "react";
 
 type Children = {
   children: JSX.Element;
 };
 
+const useStyles = makeStyles({
+  page: {
+    background: "#f9f9f9",
+    width: "100%",
+  },
+});
+
 const Layout = ({ children }: Children): ReactElement => {
-  return <div>{children}</div>;
+  const classes = useStyles();
+
+  return (
+    <div>
+      <div className={classes.page}>{children}</div>
+    </div>
+  );
 };
 export default Layout;
