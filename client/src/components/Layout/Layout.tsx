ba18b69c3810ddbb4ea7932e36675065ef58e4d1
christianturner <christiandavidphoto@gmail.com>
2021-07-23 16:15:35 -0400

    side drawer and styles

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,14 +1,25 @@
-import { makeStyles } from "@material-ui/core";
 import { ReactElement } from "react";
+import { Drawer, makeStyles, Typography } from "@material-ui/core";
 
 type Children = {
   children: JSX.Element;
 };
 
+const drawerWidth = 240;
+
 const useStyles = makeStyles({
+  root: {
+    display: "flex",
+  },
   page: {
     background: "#f9f9f9",
     width: "100%",
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
   },
 });
 
@@ -16,7 +27,18 @@ const Layout = ({ children }: Children): ReactElement => {
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <div>
+          <Typography variant="h5">Post it!</Typography>
+        </div>
+      </Drawer>
+
       <div className={classes.page}>{children}</div>
     </div>
   );
