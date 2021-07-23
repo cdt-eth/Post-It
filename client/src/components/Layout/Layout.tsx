26d0bb4b327e2690d48cb918d6f6a0d3894f96f6
christianturner <christiandavidphoto@gmail.com>
2021-07-23 16:25:24 -0400

    sidebar content

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,5 +1,15 @@
 import { ReactElement } from "react";
-import { Drawer, makeStyles, Typography } from "@material-ui/core";
+import {
+  Drawer,
+  makeStyles,
+  Typography,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@material-ui/core";
+import { AddCircleOutlineOutlined, SubjectOutlined } from "@material-ui/icons";
+import { useHistory, useLocation } from "react-router-dom";
 
 type Children = {
   children: JSX.Element;
@@ -21,13 +31,32 @@ const useStyles = makeStyles({
   drawerPaper: {
     width: drawerWidth,
   },
+  active: {
+    background: "#f4f4f4",
+  },
 });
 
 const Layout = ({ children }: Children): ReactElement => {
   const classes = useStyles();
+  const history = useHistory();
+  const location = useLocation();
+
+  const menu = [
+    {
+      text: "My Posts",
+      icon: <SubjectOutlined color="secondary" />,
+      path: "/",
+    },
+    {
+      text: "Create Note",
+      icon: <AddCircleOutlineOutlined color="secondary" />,
+      path: "/create",
+    },
+  ];
 
   return (
     <div className={classes.root}>
+      {/* Side Bar */}
       <Drawer
         className={classes.drawer}
         variant="permanent"
@@ -37,8 +66,24 @@ const Layout = ({ children }: Children): ReactElement => {
         <div>
           <Typography variant="h5">Post it!</Typography>
         </div>
+
+        {/* Menu Links */}
+        <List>
+          {menu.map((item) => (
+            <ListItem
+              key={item.text}
+              button
+              onClick={() => history.push(item.path)}
+              className={location.pathname === item.path ? classes.active : ""}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text}></ListItemText>
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
 
+      {/* Main Content */}
       <div className={classes.page}>{children}</div>
     </div>
   );
